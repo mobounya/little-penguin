1083c567c2903d968e4b4655e2c2a2fd23b42ed8
mobounya <amine.bounya20@gmail.com>
2022-06-13 22:45:13 +0100

    made assignment 05 compliant with kernel coding style standards

--- as5/my-misc.c
+++ as5/my-misc.c
@@ -1,3 +1,5 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/init.h>
@@ -12,51 +14,51 @@ MODULE_AUTHOR("mobounya");
 MODULE_DESCRIPTION("A misc device driver");
 
 ssize_t misc_dev_read(struct file *filp, char __user *buf,
-                    size_t count, loff_t *f_pos)
+			size_t count, loff_t *f_pos)
 {
-    char *login = "mobounya";
-    size_t size = 8;
-    return simple_read_from_buffer(buf, count, f_pos, login, size);
+	char *login = "mobounya";
+	size_t size = 8;
+
+	return simple_read_from_buffer(buf, count, f_pos, login, size);
 }
 
 ssize_t misc_dev_write(struct file *file, const char __user *buf,
-               size_t len, loff_t *ppos)
+			size_t len, loff_t *ppos)
 {
-    if (strcmp(buf, "mobounya") == 0)
-        return 8;
-    else
-        return -1; 
+	if (strcmp(buf, "mobounya") == 0)
+		return 8;
+	else
+		return -1;
 }
 
-struct file_operations fops = {
-    .read = misc_dev_read,
-    .write = misc_dev_write
+const struct file_operations fops = {
+		.read = misc_dev_read,
+		.write = misc_dev_write,
 };
 
-struct miscdevice misc_dev = {
-    .minor = MISC_DYNAMIC_MINOR,
-    .name = "fortytwo",
-    .fops = &fops
+const struct miscdevice misc_dev = {
+		.minor = MISC_DYNAMIC_MINOR,
+		.name = "fortytwo",
+		.fops = &fops,
 };
 
 int hello_init(void)
 {
-    int error;
+	int error;
 
-    printk("Init misc device penguin\n");
-    error = misc_register(&misc_dev);
-    if (error)
-    {
-        pr_err("misc_register failed");
-        return error;
-    }
-    return 0;
+	printk(KERN_INFO "Init misc device penguin\n");
+	error = misc_register(&misc_dev);
+	if (error) {
+		pr_err("misc_register failed");
+		return error;
+	}
+	return 0;
 }
 
 void hello_cleanup(void)
 {
-    printk("Cleaning up misc device module.\n");
-    misc_deregister(&misc_dev);
+	printk(KERN_INFO "Cleaning up misc device module.\n");
+	misc_deregister(&misc_dev);
 }
 
 module_init(hello_init);
